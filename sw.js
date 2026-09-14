b1eb15f1398819d2ab221bde0a86c41f51a71f36
Muhammet Emin İPEK <75563021+sytex10@users.noreply.github.com>
2026-09-14 09:44:49 +0300

    Perfect balanced mobile proportions v19: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gridpop-cache-v18-perfect-mobile-fit';
+const CACHE_NAME = 'gridpop-cache-v19-balanced-mobile-proportions';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
